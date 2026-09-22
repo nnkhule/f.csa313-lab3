b8a48c56f1b8e3a3ada7d09349b22f8366522a46
nnkhule <you@example.com>
2026-09-22 20:01:12 +0800

    alham 5 mini chaos

--- slo-test.js
+++ slo-test.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-  vus: 20, duration: '1m',
+  vus: 20, duration: '2m',
   thresholds: {
     'http_req_duration{name:cart}': ['p(95)<50'],   // Performance SLO
     'http_req_failed{name:pay}': ['rate<0.08'],       // Reliability SLO
